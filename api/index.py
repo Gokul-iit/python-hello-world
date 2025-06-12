836d04b38ace93331ff8417fed4c4bac3b16d351
Gokul-iit <24ds1000021@ds.study.iitm.ac.in>
2025-06-12 12:22:57 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,22 @@
-from http.server import BaseHTTPRequestHandler
+# from http.server import BaseHTTPRequestHandler
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         self.wfile.write('Hello, world!'.encode('utf-8'))
+#         return
+
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hi, this is Jerry"}
+
+# 👇 THIS is crucial
+handler = Mangum(app)
